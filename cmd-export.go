946ed1651dbd534a60247b91a3af89d32884c854
Michael Hendricks <michael@ndrix.org>
2017-05-30 15:14:43 -0400

    CmdExport: add documentation

--- cmd-export.go
+++ cmd-export.go
@@ -2,6 +2,10 @@ package hush
 
 import "io"
 
+// CmdExport writes tree t to w in a tab-separated format suitable for
+// use with "hush import" and scripting.
+//
+// This function implements "hush export".
 func CmdExport(w io.Writer, t *Tree) error {
 	for _, branch := range t.branches {
 		p, v := branch.path, branch.val
