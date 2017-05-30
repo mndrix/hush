34c07d6266200ca30508f495ece0e292128bf027
Michael Hendricks <michael@ndrix.org>
2017-05-30 15:46:41 -0400

    CmdLs: add documentation

--- cmd-ls.go
+++ cmd-ls.go
@@ -2,6 +2,9 @@ package hush
 
 import "io"
 
+// CmdLs prints to w that portion of tree which matches pattern.
+//
+// This function implements "hush ls"
 func CmdLs(w io.Writer, tree *Tree, pattern string) error {
 	tree = tree.Filter(pattern)
 	err := tree.Print(w)
