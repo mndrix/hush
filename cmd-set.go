2a8ee13ff444ec6244f1e26a251b35fc0c824365
Michael Hendricks <michael@ndrix.org>
2017-05-31 13:26:12 -0400

    CmdSet: don't allow checksum to be set manually
    I suppose this doesn't matter since setting the checksum manually
    would instantly have the changes overwritten with the old checksum.
    However, attempting to set the checksum this way is almost certainly
    indicative of a broader problem and succeeding silently is doing no
    favors.

--- cmd-set.go
+++ cmd-set.go
@@ -5,9 +5,15 @@ import (
 	"io"
 )
 
+// CmdSet sets the value for a given path in tree.
+//
+// This function implements "hush set"
 func CmdSet(w io.Writer, tree *Tree, p Path, v *Value) error {
 	if p.IsConfiguration() {
 		return errors.New("Can't set a configuration path")
+	}
+	if p.IsChecksum() {
+		return errors.New("Can't set file checksum manually")
 	}
 	tree.set(p, v)
 	t := tree.Filter(p.Parent().String())
